20615c22a122fa92260d9e4134566fad21e3a1f3
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2023-11-16 22:36:01 -0300

    Front-end and Back-end interaction

--- src/Bolseiro/Home/Home.jsx
+++ src/Bolseiro/Home/Home.jsx
@@ -1,28 +1,57 @@
-import { React } from 'react';
-import { useState } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 import Nav from '../Nav/Nav.jsx';
 
 const Home = () => {
 
+    const axios = require('axios');
+    const [search, setSearch ] = useState();
     const [items, setItems] = useState([]);
-    const [book, setBook] = useState();
+    const [dataLength, setDataLength] = useState();
+
+
+    const fetchData = async () => {
+        try {
+            const res = await fetch(`http://localhost:3000/book/`);
+            if (!res.ok) {
+                throw new Error(`Erro na requisição: ${res.status} - ${res.statusText}`);
+            }
+    
+            const data = await res.json();
+            console.log("Dados recebidos:", data);
+    
+            if (Array.isArray(data) || typeof data === 'object') {
+                const dataArray = Array.isArray(data) ? data : [data];
+                console.log("Tipo de dados é uma array ou objeto.");
+    
+                if (dataArray.length === 0) {
+                    console.log("Final da página");
+                } else if (dataLength !== dataArray.length) {
+                    setDataLength(dataArray.length);
+                    setItems(prevItems => [...prevItems, ...dataArray]);
+                }
+            } else {
+                console.error("Os dados não são do tipo esperado (array ou objeto).");
+            }
+        } catch (error) {
+            console.error("Erro na requisição:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     return(
         <div>
-            <header>
-                <Nav/>
-            </header>
-
-            <main>
-                Home
-            </main>
-
-            <footer>
-
-            </footer>
+            {items.map(item => (
+                <React.Fragment key={item.id_user}>
+                    <div><button>{item.book_name}</button></div>
+                </React.Fragment>
+            ))}
         </div>
     )
-}
+};
 
 export default Home;
--- src/Bolseiro/Nav/Nav.jsx
+++ src/Bolseiro/Nav/Nav.jsx
@@ -1,22 +1,17 @@
 import './Nav.css';
 import Lupa from './lupa.png';
+import axios from 'axios';
+import React from 'react';
+import { useState } from 'react';
 
 function Nav() {
 
-    function getSearch(search) {
-        if(search.length === 0) {
-            console.log("Nada foi pesquisado")
-        }
-        else {
-            console.log(`O que foi pesquisado: ${search}`);
-            //axios.get(`http:3000/book/findOne/${search}`);
-        }
-    }
+
 
     return(
         <div className="Nav-bar">
             <input type="text" id="search-bar" placeholder="Pesquise o livro que quiser"/>
-            <button id="search-button" onClick={() => getSearch(document.getElementById("search-bar").value)}>
+            <button id="search-button">
                 <img src={Lupa} alt="lupa" id="lupa"/>
             </button>
         </div>
