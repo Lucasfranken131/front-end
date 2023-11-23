40c9afa965c9439818e587948ae784706ecc947a
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2023-11-22 23:01:33 -0300

    feat: front install

---
+++ src/Bolseiro/Nav/Nav.jsx
@@ -0,0 +1,22 @@
+import './Nav.css';
+import Lupa from './lupa.png';
+import axios from 'axios';
+import React from 'react';
+import { useState } from 'react';
+
+function Nav() {
+
+
+
+    return(
+        <div className="Nav-bar">
+            <div>Bolseiro</div>
+            <input type="text" id="search-bar" placeholder="Pesquise o livro que quiser"/>
+            <button id="search-button">
+                <img src={Lupa} alt="lupa" id="lupa"/>
+            </button>
+        </div>
+    )
+}
+
+export default Nav;
